de12af324afd8c906a2578bc447581a0f1307eb2
KEVIN CROSS MINCHAKPU <113268445+KCMinchakpu@users.noreply.github.com>
2024-09-22 13:26:13 +0100

    commit

--- todo-list-app/src/TodoList.jsx
+++ todo-list-app/src/TodoList.jsx
@@ -4,7 +4,7 @@ import { TodoItem } from "./TodoItem"
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
-      {todos.length === 0 && "No Todos"}
+      {todos.length === 0 && "Nothing to do today"}
       {todos.map(todo => {
         return (
           <TodoItem
